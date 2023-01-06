c1034c4ae169d1f18347976f8e65c0ce0c0ed17b
Nelzon <nelzonm@gmail.com>
2023-01-06 14:29:19 +0000

    Basic REACT page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
-import Auth from "./Auth";
-import AuthContext from "./auth-context";
+// rafce
+import React from 'react'
 
 const App = () => {
-//using the state to dynamicallly pass the values to the context
+  return (
+    <div>
+      <h1> Basic REACT page</h1>
+    </div>
+  )
+}
 
-const [authstatus, setauthstatus] = useState(false);
-const login = () => {
-	setauthstatus(true);
-};
-return (
-	<React.Fragment>
-	<AuthContext.Provider value={{ status: authstatus, login: login }}>
-		<Auth />
-	</AuthContext.Provider>
-	</React.Fragment>
-);
-};
-export default App;
+export default App
