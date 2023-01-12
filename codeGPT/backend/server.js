a57319b36748f229f18ca47848a686b8a01e03ce
Nelzon <nelzonm@gmail.com>
2023-01-12 12:47:28 +0000

    Working on Roles Auth Permission

--- codeGPT/backend/server.js
+++ codeGPT/backend/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const bcrypt = require('bcrypt')
+const {
+    users
+} = require('./data')
+const projectRouter = require('./routes/projects')
 const app = express();
 
 // mongoose.connect(process.env.MONGO_URI, {
@@ -13,8 +17,13 @@ const app = express();
 // .catch(err => {
 //     console.log('Error connecting to MongoDB Atlas!', err);
 // })
-app.use(express.json())
-const users = []
+
+// MIDLEWARES
+app.use(express.json()) // to pull data from POST requests
+app.use(setUser) // setting up a user
+app.use('projects', projectRouter) // projectRouter is dedicated to path projects
+
+// const users = []
 
 app.get('/users', (req, res) => {
     res.json(users)
@@ -32,26 +41,54 @@ app.post('/users', async (req, res) => {
         }
         users.push(user)
     } catch (error) {
-        res.status(500).send({message: 'oops'})
+        res.status(500).send({
+            message: 'oops'
+        })
     }
 })
 
 app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name )
-    if(user ==  null){
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
         //return res.status(400).send('400 Bad Request. Can not find user')
-        return res.status(400).json({message: '400 Bad Request. Can not find user'})
+        return res.status(400).json({
+            message: '400 Bad Request. Can not find user'
+        })
     }
     try {
-        if(await bcrypt.compare(req.body.password, user.password)){
+        if (await bcrypt.compare(req.body.password, user.password)) {
             res.send('Success')
         } else {
             res.status(403).send('403 Forbidden, not allowed')
         }
     } catch (error) {
         res.status(500).send('500 Internal Server Error')
-    }     
+    }
 })
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+app.get('/dashboard', (req, res) => {
+    res.send('Dasboard Page')
+})
+app.get('/admin', (req, res) => {
+    res.send('Admin Page')
+})
+
+
+// middleware function
+function setUser(req, res, next) {
+    // inside req.body we are passing up a userId
+    const userId = req.body.userId
+    if (userId) {
+        //setting the user with that id passed from the req.body.userId
+        req.user = users.find(user => user.id === userId)
+        //console.log(req.user);
+    }
+    next()
+}
+
 
 const PORT = process.env.PORT || 4001
 app.listen(PORT, () => {
