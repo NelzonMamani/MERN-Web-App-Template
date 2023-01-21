478ecef498f3674aed6df308fbadff171ae48b33
Nelzon Mamani <nelzonm@gmail.com>
2023-01-21 13:23:42 +0000

    git still giving me trouble

--- ai/photo-project/frontend/src/components/NavBar.jsx
+++ ai/photo-project/frontend/src/components/NavBar.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
+import Home from "./Pages/Home";
+import Music from "./Pages/Music";
+import Photos from "./Pages/Photos";
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <NavLink to="/" className="nav-item" exact>
-        Home
-      </NavLink>
-      <NavLink to="/photos" className="nav-item">
-        Photos
-      </NavLink>
-      <NavLink to="/music" className="nav-item">
-        Music
-      </NavLink>
-      <NavLink to="/programming" className="nav-item">
-        Programming
-      </NavLink>
-      <NavLink to="/goodnews" className="nav-item">
-        Good News
-      </NavLink>
-    </div>
+    <nav className="navbar">
+      <div className="navbar">
+        <ul>
+          <NavLink to="/" className="nav-item" exact>
+            {Home}
+            Home
+          </NavLink>
+          <NavLink to="/photos" className="nav-item">
+            {Photos}
+            Photos
+          </NavLink>
+          <NavLink to="/music" className="nav-item">
+            Music
+          </NavLink>
+          <NavLink to="/programming" className="nav-item">
+            {Music}
+            Programming
+          </NavLink>
+          <NavLink to="/goodnews" className="nav-item">
+            Good News
+          </NavLink>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
